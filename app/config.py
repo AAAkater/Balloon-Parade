e51ab51cb39aeeae838e8ade3158432b03207390
AAAkater <1318382761@qq.com>
2024-11-23 13:57:23 +0800

    🐳 chore: add conf

--- app/config.py
+++ app/config.py
@@ -1,1 +1,12 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    BALLON_HOST: str
+    BALLOON_PORT: int
+
+    class Config:
+        env_file: str = ".env"
+
+
+setting = Settings()
